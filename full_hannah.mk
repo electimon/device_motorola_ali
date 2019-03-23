a128b4342e83d9a7e7fbc9e2abc8278a5b7340fa
dianlujitao <dianlujitao@lineageos.org>
2019-03-23 16:17:36 +0800

    hannah: Fix HW FDE when building with Pie tree
     * ro.board.platform is no longer part of recovery ramdisk on Pie, thus
       hw_get_module_by_class would fail to find the keystore module and HW
       FDE is broken.
     * To address the issue, use a property to specify the HAL class to make
       the HAL picked by libhardware.
    
    Change-Id: I2b09a06be34ca66a400e98fce8714226ae5f3d10

--- full_hannah.mk
+++ full_hannah.mk
@@ -25,6 +25,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcryptfs_hw
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8937
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hannah
 PRODUCT_NAME := full_hannah
