5f39fe98576b400fa3451fdf8ab40334a243c6c2
electimon <electimon@gmail.com>
2020-04-18 21:29:38 -0400

    ali: Build vibrator hal

--- device.mk
+++ device.mk
@@ -393,11 +393,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Vndk
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-sp/libbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbase-v28.so
 
 PRODUCT_PACKAGES += \
     vndk-sp
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 #Thermal
 PRODUCT_PACKAGES += \
