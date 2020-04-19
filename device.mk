ffc80732f2157ca8ab9b7b403b379d4afea0633d
electimon <electimon@gmail.com>
2020-04-18 21:06:37 -0400

    ali: Build Health

--- device.mk
+++ device.mk
@@ -157,6 +157,11 @@ PRODUCT_PACKAGES += \
     vendor.display.color@1.0-service \
     vendor.display.color@1.0-impl
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
