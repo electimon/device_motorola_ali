4610a384e1264f7a310d7f4b9b0e109e173f10a1
electimon <electimon@gmail.com>
2020-05-09 00:39:47 -0400

    ali: add some missing egl props

--- properties.mk
+++ properties.mk
@@ -93,15 +93,28 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.hwc_set_default_colormode=true \
-    debug.sf.enable_hwc_vds=1 \
+    debug.egl.hw=0 \
+    debug.enable.sglscale=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    debug.mdpcomp.logs=0 \
+    debug.sf.disable_hwc_vds=1 \
+    persist.vendor.dpm.feature=1 \
+    ro.hardware.egl=adreno \
     debug.sf.hw=0 \
-    debug.sf.latch_unsignaled=0 \
-    vendor.gralloc.enable_fb_ubwc=1 \
-    debug.sf.disable_gl_backpressure=1 \
+    debug.sf.recomputecrop=0 \
     dev.pm.dyn_samplingrate=1 \
+    debug.sdm.support_writeback=0 \
+    persist.demo.hdmirotationlock=false \
+    persist.hwc.enable_vds=1 \
+    persist.hwc.mdpcomp.enable=true \
     ro.opengles.version=196610 \
-    ro.qualcomm.cabl=0
+    ro.qualcomm.cabl=0 \
+    ro.vendor.display.cabl=2 \
+    sdm.debug.disable_skip_validate=1 \
+    vendor.display.disable_skip_validate=1 \
+    vendor.display.enable_default_color_mode=1 \
+    vendor.gralloc.enable_fb_ubwc=1 \
+    sys.display-size=1920x1080
 
 # Face Unlock
 PRODUCT_PROPERTY_OVERRIDES += \
